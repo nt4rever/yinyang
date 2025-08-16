fed140a6156ce47e42c0c9f0188d19c63e097fd2
tannnguci <levantanald@gmail.com>
2025-08-16 23:27:08 +0700

    fix nginx path

--- hooks/nginx-restart.sh
+++ hooks/nginx-restart.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo cp /usr/share/nginx/html/laravel/deployment/aws/config/nginx.default.conf /etc/nginx/conf.d/default.conf
+sudo cp /usr/share/nginx/html/laravel/deployment/aws/nginx.default.conf /etc/nginx/conf.d/default.conf
 sudo service nginx restart
