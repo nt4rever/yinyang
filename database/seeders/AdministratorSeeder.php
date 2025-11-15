525b5129420dfd669692aa15215840aefd9eeb35
nt4rever <69197323+nt4rever@users.noreply.github.com>
2025-11-15 10:53:08 +0000

    Apply automatic changes

--- database/seeders/AdministratorSeeder.php
+++ database/seeders/AdministratorSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
 class AdministratorSeeder extends Seeder
