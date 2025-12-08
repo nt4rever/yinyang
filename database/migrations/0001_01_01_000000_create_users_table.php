62e582ab324ff07861056aaf10212d79d2a737a4
tanlv <tanlv@htplus.software>
2025-12-08 13:54:28 +0700

    fix: use timestamp without timezone

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,15 +15,15 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestampTz('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar_path')->nullable();
             $table->string('lang', 10)->default('en');
             $table->string('timezone', 50)->default('UTC');
             $table->rememberToken();
             $table->unsignedInteger('lock_version')->default(0);
-            $table->timestampsTz();
-            $table->softDeletesTz();
+            $table->timestamps();
+            $table->softDeletes();
             $table->foreignUuid('created_by')->nullable();
             $table->foreignUuid('updated_by')->nullable();
             $table->foreignUuid('deleted_by')->nullable();
@@ -32,7 +32,7 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
-            $table->timestampTz('created_at')->nullable();
+            $table->timestamp('created_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
